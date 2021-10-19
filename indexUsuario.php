4bc5aea13542ec655d92aa9c60c0100cc506c357
Acurame <acuramee@gmail.com>
2021-10-19 00:59:38 -0600

    Catalogo terminado
    Se termina el catalogo para el usuario

--- indexUsuario.php
+++ indexUsuario.php
@@ -1,46 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-    <title>Document</title>
-</head>
-<body class="bg-black">
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Mira Mar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#inicio">Inico</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#servicios">Servicios</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#precios">Precio</a>
-        </li>
-      </ul>
-    </div>
-    <div class="row justify-content-end px-5">
-        <?php session_start() ?>
-        <a class="btn btn-outline-success"><?php echo $_SESSION['usuario'] ?></a>
-    </div>
-    <div class="row justify-content-end">
-        
-        <a class="btn btn-outline-success" href="Login.php">Inicio de sesion</a>
-    </div>
-  </div>
-</nav>
 
-    <div class="container-lg my-4">
+<?php include_once("./include/headerUsuari.php") ?>
+<?php include_once("./Model/db.php") ?>
+<div class="container-lg my-4">
+<div class="row row-cols-1 row-cols-md-3 g-4">
 
-
+  <?php
+  $consult = "SELECT * FROM habitaciones";
+  $result = mysqli_query($conexion,$consult);
+  
+  while($row = mysqli_fetch_array($result)){?>
+    <div class="col">
+        <div class="card">
+            <img src="/img/imagenes_abitaciones/<?php echo $row['imagenHabitacion']?>" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $row['NumeroHabitacion']?></h5>
+                <p class="card-text"><?php echo $row['DescripcionHabitacion']?></p>
+            </div>
+        </div>
     </div>
+  <?php } ?>
+</div>
+</div>
 
-</body>
+<?php include_once("./include/fooder.php") ?>
