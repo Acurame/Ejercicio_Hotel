4a805c7cf4d9e64641126fda04189300d2e64b49
JhonsHZ <62127227+JhonsHZ@users.noreply.github.com>
2021-10-19 00:17:43 -0600

    registrar
    ya esta registro con email al 100

---
+++ registro.php
@@ -0,0 +1,52 @@
+<?php include("Model/db.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container">
+        <div class="row justify-content-center pt-5 mt-5 m-1">
+            <div class="col-md-6 col-sm-8 col-xl-4 col-lg-5 formulario">
+            <form action="validarRegistro.php" method="post">
+                    <div class="form-group text-center pt-3">
+                        <h1 class="text-dark">INGRESO</h1>
+                    </div>
+                    <div class="form-group mx-sm-4 pt-3">
+                    <p>Usuario <input type="text" class="form-control" placeholder="Ingrese su Usuario" name="user"></p>
+                    </div>
+                    <div class="form-group mx-sm-4 pb-3">
+                    <p>Contraseña <input type="text" class="form-control" placeholder="Ingrese su Contraseña" name="passwd"></p>
+                    </div>
+
+                    <div class="form-group mx-sm-4 pb-3">
+                    <p>Correo <input type="text" class="form-control" placeholder="Ingrese su Correo" name="mail"></p>
+                    </div>
+                 
+                    <div class="form-group mx-sm-4 pb-2">
+                        <input type="submit" name="save" class="btn btn-block ingresar" value="Registrar">
+                    </div>
+                  
+                    </div>
+                   
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="/node_modules/sweetalert2/dist/sweetalert2.js"></script>
+    
+</body>
+</html>
---
+++ validarRegistro.php
@@ -0,0 +1,60 @@
+<?php
+
+include("Model/db.php"); 
+
+if(isset($_POST['save'])){
+    $user = $_POST['user'];
+    $password = $_POST['passwd'];
+    $email = $_POST['mail'];
+    $direcion = "../img/";
+    mkdir($direcion . $user);
+   
+    $consult = "INSERT INTO usuarios(NombreUsuario, password, Correo) VALUES ('$user','$password','$email')";
+    $error = mysqli_query($conexion,$consult);
+
+        if(!$error){
+            die("Error");
+        }
+    
+}
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try{
+
+    //Server settings
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'pruebaproy23@gmail.com';                 // SMTP username
+    $mail->Password = 'jh5587309';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+    //Recipients
+    $mail->setFrom('pruebaproy23@gmail.com', 'Hotel Mira Mar');
+    $mail->addAddress($email);     // Add a recipient
+  //  $mail->addReplyTo('jhon56489754@gmail.com');
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Bienvenidos a Hotel Mira Mar';
+   $mail->Body = "Nombre de usuario es: ". $user . "\nla contra es: " . $password;
+    //$mail->Body = $password;
+	
+
+ //   $mail->Body    = $mailContent ;
+    $mail->send();
+    echo 'El mensaje se envio correctamente';
+}
+catch (Exception $e){
+    echo 'Hubo un error ', $mail->ErrorInfo;
+}
+
+
+header("location:../index.php");
+// include("../index.php"); 
+
+?>
