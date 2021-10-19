5b69d347fa2659287d719087849322e0f3666402
Acurame <acuramee@gmail.com>
2021-10-19 08:30:14 -0600

    ejemplo

---
+++ indexEmpleado.php
@@ -0,0 +1,8 @@
+
+<?php include_once("./include/headerUsuari.php") ?>
+<?php include_once("./Model/db.php") ?>
+<div class="container-lg my-4">
+<h1>Bienvenidos</h1>
+</div>
+
+<?php include_once("./include/fooder.php") ?>
