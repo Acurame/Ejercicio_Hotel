dbdc5afd0fbfb3da43c6786c0cb5637d00e38969
Acurame <acuramee@gmail.com>
2021-11-09 21:15:03 -0600

    Actualizacion

--- index.php
+++ index.php
@@ -4,133 +4,178 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+    <title>Hotel Mira Mar</title>
 </head>
-<body class="bg-black">
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Mira Mar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#inicio">Inico</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#servicios">Servicios</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#precios">Precio</a>
-        </li>
-      </ul>
-    </div>
-    <div class="row justifyc-content-end px-1">
-      <a class="btn btn-outline-success " href="Login.php">Inicio de sesion</a>
-    </div>
-    <div class="row justify-content-end px-5">
-        <a class="btn btn-outline-success" href="registro.php">Registro</a>
-    </div>
-  </div>
+<body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+        <div class="container">
+            <a href="#" class="navbar-brand">Hotel MiraMar</a>
+            <button type="button" class="navbar-toggler" data-bs-target="#navbarNav" data-bs-toggle="collapse" aria-controls="navbarNav" aria-expanded="false" aria-lable="Toggle Navbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="mx-auto"></div>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">Incio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#sobre" class="nav-link text-white">Sobre nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#Servicios" class="nav-link text-white">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#Contactenos" class="nav-link text-white">Contactanos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/Login.php" class="nav-link text-white">Inicio de Sesión</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/registro.php" class="nav-link text-white">Registro</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
 </nav>
-    <div class="container-lg my-4" id="inicio">
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-            <img src="img/1.jpg" class="d-block w-100" alt="...">
+    <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+        <div class="content text-center">
+            <h1 class="text-white">Hotel Mira Mar</h1>
+        </div>
+    </div>
+
+
+    <section class=" container my-5 d-grid gap-5" id="sobre">
+        <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+            <div class="col-lg-6"><img class="img-fluid" src="/img/Tarjetas/1.jpg" alt="..." /></div>
+            <div class="col-lg-6 order-lg-first">
+                <div class="bg-black text-center h-100 project">
+                    <div class="d-flex h-100">
+                        <div class="project-text w-100 my-auto text-center text-lg-left">
+                            <h1 class="text-white">Vista al mar</h1>
+                            <p class="mb-0 text-white-50 fs-4">Disfruta del hermoso y relajante paisaje desde el balcón de nuestras habitaciones, ya sea para apreciar un vigorizante amanecer o un romántico ocaso. <br> ¡No te arrepentirás!</p>
+                            <hr class="d-none d-lg-block mb-0 ms-0" />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="carousel-item">
-            <img src="img/2.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="row gx-0 justify-content-center ">
+                <div class="col-lg-6"><img class="img-fluid" src="/img/Tarjetas/2.jpg" alt="..." /></div>
+                <div class="col-lg-6 order-lg-first">
+                    <div class="bg-black text-center h-100 project">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-right">
+                                <h1 class="text-white">Instalaciones Cinco Estrellas</h1>
+                                <p class="mb-0 text-white-50 fs-4">Además de la impresionante vista al mar, disfruta de nuestras instalaciones que ofrecen comodidad y lujo al mejor estilo que te mereces.</p>
+                                <hr class="d-none d-lg-block mb-0 me-0" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="carousel-item">
-            <img src="img/3.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="row gx-0 justify-content-center ">
+                <div class="col-lg-6"><img class="img-fluid" src="/img/Tarjetas/3.jpg" alt="..." /></div>
+                <div class="col-lg-6 order-lg-first">
+                    <div class="bg-black text-center h-100 project">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-right">
+                                <h1 class="text-white">¡Lujo, Diversión y Relajación!</h1>
+                                <p class="mb-0 text-white-50 fs-4">¿Cansado de explorar los alrededores? Te ofrecemos servicio de spa, donde podrás relajarte. ¿Hambriento? ¡No te preocupes! Contamos con un restaurant cinco estrellas con los mejores platillos a tu disposición.</p>
+                                <hr class="d-none d-lg-block mb-0 me-0" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        </div>
-    </div>
+    </section>
 
-    <div class="container-lg my-6 " id="servicios">
-    <div class="card-group">
-  <div class="card">
-    <img src="img/Card-2.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Vista al mar</h5>
-      <p class="card-text">Disfruta del hermoso y relajante paisaje desde el balcón de nuestras habitaciones, ya sea para apreciar un vigorizante amanecer o un romántico ocaso. ¡No te arrepentirás!</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="img/Card-1.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Instalaciones Cinco Estrellas</h5>
-      <p class="card-text">Además de la impresionante vista al mar, disfruta de nuestras instalaciones que ofrecen comodidad y lujo al mejor estilo que te mereces.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="img/Card-3.jpg" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">¡Lujo, Diversión y Relajación!</h5>
-      <p class="card-text fs-4">¿Cansado de explorar los alrededores? Te ofrecemos servicio de spa, donde podrás relajarte. ¿Hambriento? ¡No te preocupes! Contamos con un restaurant cinco estrellas con los mejores platillos a tu disposición.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
-    </div>
+    <section class="page-section" id="Servicios">
+            <div class="container">
+                <div class="text-center p-4">
+                    <h1 class="section-heading text-uppercase p-4">Servicios</h1>
+                </div>
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <img src="/img/icons/menu.png" alt="">
+                        </span>
+                        <h4 class="my-3">Comida</h4>
+                        <p class="text-muted">Ecónomica y sabrosa, visita nuestro restaurante y pide el menú del día. ¡Descubre deliciosas sorpresas y promociones.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                        <img src="/img/icons/Servicios.png" alt="">
+                        </span>
+                        <h4 class="my-3">Servicios a tu Alcance.</h4>
+                        <p class="text-muted">Accesible y al alcance de tu bolsillo, permítete disfrutar de los servicios que ofrecemos en el spa y nuestro exclusivo servicio de jacuzzi..</p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                        <img src="/img/icons/quetzal.png" alt="">
+                        </span>
+                        <h4 class="my-3">¡Los Mejores Precios!</h4>
+                        <p class="text-muted">¡Te ofrecemos la mejor calidad, los precios mas competitivos en el mercado y la calidad más alta. ¡Disfruta de las mejores vacaciones que la vida puede ofrecer!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    
+        <section>
+            <div class="banner-image2 w-100 vh-100 d-flex justify-content-center align-items-center" id="Contactenos">
+                <div class="container px-4 px-lg-5">
+                    <div class="row gx-4 gx-lg-5">
+                        <div class="col-md-4 mb-3 mb-md-0">
+                            <div class="card text-white bg-dark py-4 h-100">
+                                <div class="card-body text-center">
+                                    <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                                    <h4 class="text-uppercase m-0">Dirección</h4>
+                                    <hr class="my-4 mx-auto" />
+                                    <div class="small text-white-50">480 de El Oso Feliz</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-3 mb-md-0">
+                            <div class="card text-white bg-dark py-4 h-100">
+                                <div class="card-body text-center">
+                                    <i class="fas fa-envelope text-primary mb-2"></i>
+                                    <h4 class="text-uppercase m-0">Email</h4>
+                                    <hr class="my-4 mx-auto" />
+                                    <div class="small text-white-50 "><a href="#!" class="link-secondary">hotelmiramar@miramar.com.gt</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-3 mb-md-0">
+                            <div class="card text-white bg-dark py-4 h-100">
+                                <div class="card-body text-center">
+                                    <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                    <h4 class="text-uppercase m-0">Phone</h4>
+                                    <hr class="my-4 mx-auto" />
+                                    <div class="small text-white-50">+502 9028-8832</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
 
-    <div class="container-lg my-4" id="precios">
-    <div class="card mb-4" style="max-width: 940px;">
-    <div class="row g-0">
-        <div class="col-md-4">
-        <img src="img/Money-1.jpg" class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-        <div class="card-body">
-            <h1 class="card-title">Comida</h1>
-            <p class="card-text fs-4">Ecónomica y sabrosa, visita nuestro restaurante y pide el menú del día. ¡Descubre deliciosas sorpresas y promociones.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-        </div>
-    </div>
-    </div>
-    </div>
-
-    <div class="container-lg my-5">
-    <div class="card mb-4" style="max-width: 940px;">
-    <div class="row g-0">
-        <div class="col-md-8">
-        <div class="card-body">
-            <h1 class="card-title">Servicios a tu Alcance.</h1>
-            <p class="card-text fs-4">Accesible y al alcance de tu bolsillo, permítete disfrutar de los servicios que ofrecemos en el spa y nuestro exclusivo servicio de jacuzzi..</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-        </div>
-        <div class="col-md-4">
-        <img src="img/Money-2.jpg" class="img-fluid rounded-start" alt="...">
-        </div>
-    </div>
-    </div>
-    </div>
-
-    <div class="container-lg my-4">
-    <div class="card mb-4" style="max-width: 940px;">
-    <div class="row g-0">
-        <div class="col-md-4">
-        <img src="img/Money-3.jpg" class="img-fluid rounded-start" alt="...">
-        </div>
-        <div class="col-md-8">
-        <div class="card-body">
-            <h1 class="card-title">¡Los Mejores Precios!</h1>
-            <p class="card-text fs-4">¡Te ofrecemos la mejor calidad, los precios mas competitivos en el mercado y la calidad más alta. ¡Disfruta de las mejores vacaciones que la vida puede ofrecer!</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        </div>
-        </div>
-    </div>
-    </div>
-    </div>
-
-    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <script>
+        var nav = document.querySelector('nav');
+        window.addEventListener('scroll', function(){
+            if(window.pageYOffset > 100){
+                nav.classList.add('bg-dark', 'shadow');
+                nav.classList.add('bg-dark', 'shadow');
+            }else{
+                nav.classList.remove('bg-dark', 'shadow');
+            }
+        });
+    </script>
 </body>
-</html>
+</html> 
