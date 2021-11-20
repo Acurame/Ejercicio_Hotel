ab08031bd9045a1dfaf51125fdea01b910346b80
Acurame <acuramee@gmail.com>
2021-11-19 22:13:29 -0600

    Actualizacion de errores

--- views/Clientes/home.php
+++ views/Clientes/home.php
@@ -1,8 +1,8 @@
-<?php include("../../include/header.php") ?>
+<?php include("../../assets/include/header.php") ?>
 
 <div class="container">
     <h1>Bienvenidos</h1>
 </div>
 
 
-<?php include("../../include/fooder.php") ?>
+<?php include("../../assets/include/fooder.php") ?>
