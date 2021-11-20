0d2502ae2a2e18eda262eee04dfa0903523e31a8
Acurame <acuramee@gmail.com>
2021-11-19 21:57:09 -0600

    Actualizacion de rutas del home

---
+++ views/Clientes/home.php
@@ -0,0 +1,8 @@
+<?php include("../../include/header.php") ?>
+
+<div class="container">
+    <h1>Bienvenidos</h1>
+</div>
+
+
+<?php include("../../include/fooder.php") ?>
---
+++ views/Empleados/home.php
@@ -0,0 +1,9 @@
+<?php include("../../include/header.php") ?>
+
+<div class="container">
+
+    <h1>Bienvenidos</h1>
+</div>
+
+
+<?php include("../../include/fooder.php") ?>
