8181fde48f518a6e6fdc63626f529d5d27f3d0ed
Acurame <acuramee@gmail.com>
2021-11-19 21:41:32 -0600

    actualizar

---
+++ assets/include/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+    <title>Hotel Mira Mar</title>
+</head>
+<body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark p-md-3">
+        <div class="container">
+            <a href="/index.php" class="navbar-brand">Hotel MiraMar</a>
+            <button type="button" class="navbar-toggler" data-bs-target="#navbarNav" data-bs-toggle="collapse" aria-controls="navbarNav" aria-expanded="false" aria-lable="Toggle Navbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="mx-auto"></div>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">Incio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#sobre" class="nav-link text-white">Sobre nosotros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#Servicios" class="nav-link text-white">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#Contactenos" class="nav-link text-white">Contactanos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/Login.php" class="nav-link text-white">Inicio de Sesión</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/registro.php" class="nav-link text-white">Registro</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</nav>
--- include/fooder.php
+++
@@ -1,3 +0,0 @@
-<script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
-<script src="/node_modules/sweetalert2/dist/sweetalert2.js"></script>
-</body>
