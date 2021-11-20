20e883e95f42dc40c67517d736f5e22e438ade7c
Acurame <acuramee@gmail.com>
2021-11-19 21:18:24 -0600

    Reorganizar

--- indexEmpleado.php
+++
@@ -1,8 +0,0 @@
-
-<?php include_once("./include/headerEmpleado.php") ?>
-<?php include_once("./Model/db.php") ?>
-<div class="container-lg my-4">
-<h1>Bienvenidos</h1>
-</div>
-
-<?php include_once("./include/fooder.php") ?>
